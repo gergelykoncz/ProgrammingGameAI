bd702d1768ce51a67ca8bc393e8548083b6d3c30
Gergely Koncz <gergelykoncz@Gergelys-MacBook-Pro.local>
2014-03-28 14:42:20 +0100

    Initial Commit

---
+++ WestWorld/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  WestWorld
+//
+//  Created by Gergely Koncz on 28/03/2014.
+//  Copyright (c) 2014 Gergely Koncz. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
